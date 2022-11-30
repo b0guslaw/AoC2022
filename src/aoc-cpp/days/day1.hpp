fdb2184a6b3c919481cf1b1955ec3cbde26dd349
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2022-11-30 10:34:42 +0100

    prep: Various changes Raise compiler warnings to fail on error Abstract Runner Function further Add maybe unused attrib

--- src/aoc-cpp/days/day1.hpp
+++ src/aoc-cpp/days/day1.hpp
@@ -5,11 +5,11 @@
 
 namespace aoc {
 namespace Day1 {
-uint64_t Part1(const std::vector<int>& data) {
+uint64_t Part1([[maybe_unused]] const std::vector<int>& data) {
 	return 0;
 }
 
-uint64_t Part2(const std::vector<int>& data) {
+uint64_t Part2([[maybe_unused]]const std::vector<int>& data) {
 	return 0;
 }
 }
--- src/aoc-cpp/main.cpp
+++ src/aoc-cpp/main.cpp
@@ -23,7 +23,7 @@ auto print = [](const uint64_t part1, double elapsed1, const uint64_t part2, dou
 };
 
 template<typename F, typename I>
-std::tuple<uint64_t, double> Runner(F func, std::vector<I> input) {
+std::tuple<uint64_t, double> Runner(F func, const std::vector<I> input) {
 		auto start = std::chrono::high_resolution_clock::now();
 		uint64_t result = func(input);
 		auto end = std::chrono::high_resolution_clock::now();
@@ -31,15 +31,15 @@ std::tuple<uint64_t, double> Runner(F func, std::vector<I> input) {
 		return std::make_tuple(result, elapsed);
 }
 
-template<typename T>
-void DayRunner(std::vector<T> data) {
-	auto [result1, elapsed1] = Runner(aoc::Day1::Part1, data);
-	auto [result2, elapsed2] = Runner(aoc::Day1::Part2, data);
+template<typename T, typename F1, typename F2>
+void DayRunner(F1 f1, F2 f2, const std::vector<T>& data) {
+	auto [result1, elapsed1] = Runner(f1, data);
+	auto [result2, elapsed2] = Runner(f2, data);
     static uint64_t day_count{0};
     fmt::print("Day {} results:\n" , ++day_count);
 	print(result1, elapsed1, result2, elapsed2);
 }
 
 int main() {
-    DayRunner<int>(Input::GetData<int>(INPUT_PATH + "day1.txt", '\n'));
+    DayRunner<int>(aoc::Day1::Part1, aoc::Day1::Part2, Input::GetData<int>(INPUT_PATH + "day1.txt", '\n'));
 }
