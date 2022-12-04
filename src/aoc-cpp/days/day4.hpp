ce2ce68591e2c4afe154a71530641ee0dfb4b5e5
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2022-12-04 15:01:47 +0100

    Day 4 refactor

--- src/aoc-cpp/days/day4.hpp
+++ src/aoc-cpp/days/day4.hpp
@@ -29,14 +29,21 @@ inline bool contains(int left_first, int left_last, int right_first, int right_l
     return false;
 }
 
-std::uint64_t Part1([[maybe_unused]] const std::vector<std::string>& data) {
-    std::uint64_t sum{0};
-    for (auto& line : data) {
-        int l_first{}, l_last{}, r_first{}, r_last{};
+inline void get_ranges(const std::string& line,
+                        int& l_first, int& l_last,
+                        int& r_first, int& r_last)
+{
         std::from_chars(line.data(), line.data() + line.find("-"), l_first);
         std::from_chars(line.data() + line.find_first_of("-") + 1, line.data() + line.find(","), l_last);
         std::from_chars(line.data() + line.find(",") + 1, line.data() + line.find_last_of("-"), r_first);
         std::from_chars(line.data() + line.find_last_of("-") + 1, line.data() + line.size() - (line.find_last_of("-") + 1), r_last);
+}
+
+std::uint64_t Part1([[maybe_unused]] const std::vector<std::string>& data) {
+    std::uint64_t sum{0};
+    for (auto& line : data) {
+        int l_first{}, l_last{}, r_first{}, r_last{};
+        get_ranges(line, l_first, l_last, r_first, r_last);
         if (contains(l_first, l_last, r_first, r_last)) {
             sum++;
         }
@@ -48,10 +55,7 @@ std::uint64_t Part2([[maybe_unused]] const std::vector<std::string>& data) {
     std::uint64_t sum{0};
     for (auto& line : data) {
         int l_first{}, l_last{}, r_first{}, r_last{};
-        std::from_chars(line.data(), line.data() + line.find("-"), l_first);
-        std::from_chars(line.data() + line.find_first_of("-") + 1, line.data() + line.find(","), l_last);
-        std::from_chars(line.data() + line.find(",") + 1, line.data() + line.find_last_of("-"), r_first);
-        std::from_chars(line.data() + line.find_last_of("-") + 1, line.data() + line.size() - (line.find_last_of("-") + 1), r_last);
+        get_ranges(line, l_first, l_last, r_first, r_last);
         if (overlaps(l_first, l_last, r_first, r_last)) {
             sum++;
         }
