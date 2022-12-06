f45a2ea93b42832fda3d8ede8ee88ac3c63a4db7
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2022-12-06 22:19:36 +0100

    Day 6 - change to template

--- src/aoc-cpp/days/day6.hpp
+++ src/aoc-cpp/days/day6.hpp
@@ -1,28 +1,25 @@
 #pragma once
 
-#include <atomic>
-#include <bit>
-#include <bits/ranges_algo.h>
-#include <bits/ranges_util.h>
 #include <cstdint>
 #include <numeric>
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <bitset>
 #include <bit>
 
 namespace aoc {
 namespace Day6 {
 
-std::uint64_t find_marker(const std::string& s, size_t window) {
-    
-    for (size_t i = window; i < s.size() - window; i++) {
+template<std::int32_t N>
+std::uint64_t find_marker(const std::string& s) {
+    constexpr auto a = static_cast<int32_t>('a');
+
+    for (size_t i = N; i < s.length() - N; i++) {
         std::uint32_t mask{0};
-        for (auto j{i - window}; j < i; j++) {
-            mask |= 1 << (s[j] - 'a');
+        for (auto j{i - N}; j < i; j++) {
+            mask |= 1 << (s[j] - a);
         }
-        if (std::popcount(mask) == static_cast<int32_t>(window)) {
+        if (std::popcount(mask) == N) {
             return i;
         }
     }
@@ -30,11 +27,11 @@ std::uint64_t find_marker(const std::string& s, size_t window) {
 }
 
 std::uint64_t Part1([[maybe_unused]] const std::string& data) {    
-    return find_marker(data, 4);
+    return find_marker<4>(data);
 }
 
 std::uint64_t Part2([[maybe_unused]] const std::string& data) {
-    return find_marker(data, 14);
+    return find_marker<14>(data);
 }
 }
 }
