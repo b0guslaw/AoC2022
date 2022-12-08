74fcde33d6b6daa0c65148d3fdbd52b2c3404ae9
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2022-12-08 21:02:43 +0100

    Day 8 refactor

--- src/aoc-cpp/days/day8.hpp
+++ src/aoc-cpp/days/day8.hpp
@@ -15,8 +15,8 @@ namespace Day8 {
 
 bool left_visible(const std::vector<int>& row, size_t pos) {
     bool visible{true};
-    for (size_t i{0}; i < pos; i++) {
-        if (row[i] >= row[pos]) {
+    for (size_t i{pos}; i > 0; i--) {
+        if (row[i - 1] >= row[pos]) {
             visible = false;
             break;
         }
@@ -26,8 +26,8 @@ bool left_visible(const std::vector<int>& row, size_t pos) {
 
 bool right_visible(const std::vector<int>& row, size_t pos) {
     bool visible{true};
-    for (size_t i{pos + 1}; i < row.size(); i++) {
-        if (row[i] >= row[pos]) {
+    for (size_t i{pos}; i < row.size() - 1; i++) {
+        if (row[i + 1] >= row[pos]) {
             visible = false;
             break;
         }
@@ -37,8 +37,8 @@ bool right_visible(const std::vector<int>& row, size_t pos) {
 
 bool top_visible(const std::vector<std::vector<int>>& grid, size_t row, size_t col) {
     bool visible{true};
-    for (size_t i{0}; i < row; i++) {
-        if (grid[i][col] >= grid[row][col]) {
+    for (size_t i{row}; i > 0; i--) {
+        if (grid[i - 1][col] >= grid[row][col]) {
             visible = false;
             break;
         }
@@ -48,8 +48,8 @@ bool top_visible(const std::vector<std::vector<int>>& grid, size_t row, size_t c
 
 bool bottom_visible(const std::vector<std::vector<int>>& grid, size_t row, size_t col) {
     bool visible{true};
-    for (size_t i{row + 1}; i < grid.size(); i++) {
-        if (grid[i][col] >= grid[row][col]) {
+    for (size_t i{row}; i < grid.size() - 1; i++) {
+        if (grid[i + 1][col] >= grid[row][col]) {
             visible = false;
             break;
         }
@@ -66,15 +66,14 @@ std::uint64_t Part1([[maybe_unused]] const std::vector<std::vector<int>>& data) 
 
     for (size_t i{1}; i < data.size() - 1; i++) {
         for (size_t j{1}; j < data[i].size() - 1; j++) {
-            if (left_visible(data[i], j) ||
-                right_visible(data[i], j)||
-                top_visible(data, i, j)  ||
+            if (left_visible(data[i], j)  ||
+                right_visible(data[i], j) ||
+                top_visible(data, i, j)   ||
                 bottom_visible(data, i, j)) {
                     ++sum;
-                }
+            }
         }
     }
-
     return sum;
 }
 
